7c63325b74e14259ebdb37267c54df8bfef788df
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-08-20 11:04:29 +0300

    Add the Debounce function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { getUsers } from "@/lib/getUsers"
 import { useQuery } from "@tanstack/react-query"
-
+import { useState } from "react"
+import { useDebounce } from "use-debounce"
 export default function Home() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["users"],
@@ -10,11 +13,30 @@ export default function Home() {
   })
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
+  const [value, setValue] = useState()
+  const [debouncedValue] = useDebounce(value, 1000)
+  function handleDebounce(e: any) {
+    setValue(e.target.value)
+  }
   return (
-    <main>
-      {data?.map((item) => (
+    <main className="mx-4 mt-4 lg:w-[40%]">
+      <Label htmlFor="com">Company Name</Label>
+      <Input
+        id="com"
+        placeholder="please and any value"
+        value={value}
+        onChange={handleDebounce}
+        className="mt-3 "
+      />
+
+      {/* Testing the Debounce :     
+        <p>Actual value: {value}</p>
+      <p>Debounce value: {debouncedValue}</p> */}
+
+      {/* Fetching the Data
+          {data?.map((item) => (
         <div key={item.address.geo.lat}>{item.company.name}</div>
-      ))}
+      ))} */}
     </main>
   )
 }
